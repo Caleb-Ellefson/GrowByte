f9cee7b5790c3707e40fbb897324eeaf8ae14583
caleb_ellefson <calebroger@gmail.com>
2025-09-22 20:18:42 -0500

    routes: authRouter.js: Removed old hub auth logic
    Removed old hub auth routes and api key gen
    
    Signed-off-by: caleb_ellefson <calebroger@gmail.com>

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,8 +5,6 @@ import {
   login,
   logout,
   register,
-  registerHub,
-  generateNewApiKey,
 } from '../controllers/authController.js';
 import { authenticateUser } from '../middleware/authMiddleware.js';
 import {
@@ -22,18 +20,5 @@ router.post('/login', validateLoginInput, login);
 
 // User logout (requires authentication)
 router.get('/logout', authenticateUser, logout);
-
-// HUB registration (requires admin permission)
-router.post(
-  '/register-hub',
-  registerHub
-);
-
-// Generate a new API key for a HUB (requires admin permission)
-router.post(
-  '/generate-api-key',
-  authenticateUser,
-  generateNewApiKey
-);
 
 export default router;
